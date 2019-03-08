3afe482fdbbbc59a182b96d0575b1583575365e4
Solomon Reta <45945188+dirdawa@users.noreply.github.com>
2019-03-08 15:07:17 -0600

    Add files via upload

---
+++ FinalProject/Base/IWidget.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject
+{
+    public interface IWidget
+    {
+        WidgetColor Colortype { get; }
+        int Springs { get; }
+        int Levers { get; }
+        int Gears { get; }
+        int Quantity { get; set; }
+
+    }
+}
---
+++ FinalProject/Base/WidgetSmall.cs
@@ -0,0 +1,27 @@
+﻿using FinalProject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base
+{
+    public class WidgetSmall : AbstractWidget
+    {
+        private int _gears = 2, _springs = 3, _levers = 1, _quantity;
+        private WidgetColor _widgetColor;
+
+        public override int Quantity
+        {
+            set
+            {
+                this._quantity = Quantity;
+            }
+            get
+            {
+                return _quantity;
+            }
+        }
+    }
+}
